3285a11256b2b94a839ca1880cda767413d67624
bryan <bryan.lee.jh@gmail.com>
2020-03-25 20:57:36 +0800

    basic code splitting

--- src/App.js
+++ src/App.js
@@ -3,37 +3,50 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: "page1"
+      route: "page1",
+      component: ""
     };
   }
 
   onRouteChange = route => {
-    this.setState({ route: route });
+    // no codesplitting
+    // this.setState({ route: route });
+    // with codesplitting
+    if (route === 'page1') {
+      this.setState({ route: route });
+    } else if ( route === 'page2') {
+      import('./components/Page2').then((Page2) => {
+        this.setState({ route: route, component: Page2.default });
+      });
+    } else if ( route === 'page3') {
+      import('./components/Page3').then((Page3) => {
+        this.setState({ route: route, component: Page3.default });
+      })
+    }
   };
 
   render() {
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page3') {
+    //   return <Page3 onRouteChange={this.onRouteChange} />
+    // }
+
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page3') {
-      return <Page3 onRouteChange={this.onRouteChange} />
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
     }
 
-    return (
-      <div className="App">
-        <Page1 onRouteChange={this.onRouteChange} />
-        <Page2 onRouteChange={this.onRouteChange} />
-        <Page3 onRouteChange={this.onRouteChange} />
-      </div>
-    );
   }
 }
 
